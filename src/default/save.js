fc62b1eaf331c11813ac8183f5de29426ebe147f
Sergio Lecuona <serlecu@hotmail.com>
2024-01-19 15:09:09 +0100

    start testing chart display

--- src/default/save.js
+++ src/default/save.js
@@ -6,6 +6,39 @@
  */
 import { useBlockProps } from '@wordpress/block-editor';
 
+import Chart from 'chart.js/auto';
+import { getRelativePosition } from 'chart.js/helpers';
+// import bootstrap
+
+
+const chart = new Chart(
+    document.getElementById('myChart'),
+    {
+        type: 'line',
+        data: {
+            datasets: [
+                {
+                    label: 'Category A',
+                    data: [10, 20, 30, 40],
+                    // backgroundColor: [pallette['red']],
+    
+                },
+                {
+                    label: 'Category B',
+                    data: [10, 40, 20, 30],
+                    // backgroundColor: [pallette['greyUPV']],
+                }
+            ],
+            labels: ['Red', 'Blue', 'Yellow', 'Green']
+        },
+        options:  {
+            maintainAspectRatio: false,
+    
+            borderWidth: 1
+        },
+        plugins: []
+});
+
 /**
  * The save function defines the way in which the different attributes should
  * be combined into the final markup, which is then serialized by the block
@@ -17,8 +50,13 @@ import { useBlockProps } from '@wordpress/block-editor';
  */
 export default function save() {
 	return (
-		<p { ...useBlockProps.save() }>
-			{ 'Vinv Webcharts – hello from the saved content!' }
-		</p>
+		<div class="main" { ...useBlockProps.save() }>
+			<p >
+				{ 'Vinv Webcharts – hello from the saved content!' }
+			</p>
+			<div id="chart-wrapper" class="col-12">
+				<canvas id="myChart" class=""></canvas>
+			</div>
+		</div>
 	);
 }
